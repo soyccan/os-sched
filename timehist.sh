2eb170361b28978967d100c19435682c1db5682b
soyccan <soyccan@gmail.com>
2020-04-28 22:36:28 +0800

    Remove things not related to demo

--- timehist.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-if [ -z "$1" ]; then
-    echo "Usage: $0 <pid_of_scheduler>"
-    exit 255
-fi
-sudo perf sched timehist -C 0 | awk "/$1/ { print \"\x1b[31m\" \$0 \"\x1b[0m\" } !/$1/ { print \$0 }" | less
